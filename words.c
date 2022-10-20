5ccc3bbabf52058dfce667550d21cb664cd3a2b7
Matthew Nuttall <23068292@student.uwa.edu.au>
2022-10-21 02:00:07 +0800

    altered hashtable to store absolute pathname

--- words.c
+++ words.c
@@ -1,10 +1,11 @@
-/**
+/**:
  * This is to validate
  * the length of the
  * words in the file
  */
 #include "trove.h"
 #include <ctype.h>
+#include <stdbool.h>
 
 // JUST A NULL POINTER FOR THE LIST
 LIST *list_new(void){
@@ -32,6 +33,7 @@ LIST *list_new_item(char *newWord){
         return new;
 }
 
+// ADDS TO EXISTING LIST
 LIST *list_add(LIST *list, char *newstring){
         if(list_find(list, newstring)){
                 return list;
@@ -43,11 +45,8 @@ LIST *list_add(LIST *list, char *newstring){
         }
 }
 
-// ITERATES THROUGH THE GIVEN ARRAY AND VERIFIES THE GIVEN WORD
-// IS NOT CONTAINED WITHIN THE ARRAY
-
 bool notInArray(char *word, char *wordsArr[], int numWords) {
-        for(int i = 0; i < numWords; i ++){
+        for(int i = 0; i < numWords ; i ++){
                 if(strlen(wordsArr[i]) == strlen(word)){
                         if(strcmp(wordsArr[i], word) == 0){
                                 return false;
@@ -57,10 +56,11 @@ bool notInArray(char *word, char *wordsArr[], int numWords) {
         return true;
 }
 
+// PRINTS OUT EACH ITEM IN THE LIST
 void list_print(LIST *list){
         if(list != NULL){
                 while(list != NULL){
-                        printf("%s", list->word);
+                        printf("%s\n", list->word);
                         if(list->nextWord != NULL){
                                 printf("->");
                         }
@@ -70,58 +70,45 @@ void list_print(LIST *list){
         }
 }
 
-void words(char *fileContents){
+// return the list of unique words for each file
+void words(char *fileContents, char *fileName){
         // INITIALIZE AND DECLARE SIZE OF FILE CONTENT STRING
         int i = 0;
         char character = fileContents[i];
         char *currentWord;
 
-        // CONSIDER CASE THAT ALL THE WORDS IN THE FILE ARE UNIQUE AND MIN LENGTH
-        // FOR THIS CASE NUM OF UNIQUE WORDS = NUMBER OF CHAR / (MINLENGTH-1)
-        // -1 ACCOUNTS FOR THE NON ALPHANUMERIC CHAR
-
-        // MAKING NEW LIST
-        char *uniqueWords[strlen(fileContents) / (minLength - 1)];
-        int numUniqueWords = 0;
-
         // INITIALIZES currentWord TO BE A SINGLE CHARACTER STRING
         currentWord = (char *) malloc(sizeof(char));
 
+        char *uniqueWords[strlen(fileContents)/(minLength-1)];
+        int numUniqueWords = 0;
+
         // WHILE NOT NULL BYTE
         while(character != '\0'){
-                // IF ALNUM CHAR BUILD UP THE WORD
+                // IF ALNUM CHAR, BUILD UP THE WORD
                 if(isalnum(character)){
-                        // INCREASES THE SIZE OF THE CURRENT WORD STRIGN BY ONE CHAR
+                        // INCREASES THE SIZE OF THE CURRENT WORD STRING BY ONE CHAR
                         currentWord = (char *) realloc(currentWord, sizeof(currentWord) + sizeof(char));
                         strncat(currentWord, &character, 1);
                 }
                 else{
-                        // IF LENGTH OF WORD IS GREATER THAN MINLENGTH AND NOT IN THE LIST
-                        if(strlen(currentWord) >= minLength && notInArray(currentWord, uniqueWords,numUniqueWords)) {
+                        // IF LENGTH OF WORD IS GREATER THAN MINLENGTH
+                        if(strlen(currentWord) >= minLength && notInArray(currentWord, uniqueWords, numUniqueWords)) {
                                 int wordLength = strlen(currentWord);
-                                // ALLOCATE MEMORY IN UNIQUEWORDS ARRAY FOR THE CURRENT WORD
-                                uniqueWords[numUniqueWords] = (char *) malloc(sizeof(char) * (wordLength + 1));
-                                // COPIES CURRENTWORD INTO UNIQUE WORDS ARRAY
+                                uniqueWords[numUniqueWords] = (char*) malloc(sizeof(char) * (wordLength + 1));
                                 strcpy(uniqueWords[numUniqueWords], currentWord);
                                 numUniqueWords++;
                         }
-
-                        // RESETS CURRENT WORD STRING
                         memset(currentWord, 0, strlen(currentWord));
                 }
                 // MOVES TO NEXT CHAR
                 i++;
                 character = fileContents[i];
         }
-
-        // CREATING A LIST
-        LIST *wordList = list_new();
-
-        // ADD CONTENTS OF ARRAY TO THE LIST
-        for(int w = 0; w < numUniqueWords; w++){
-                wordList = list_add(wordList, uniqueWords[w]);
+        char buf[PATH_MAX];
+        char *absPath = realpath(fileName, buf);
+        // HASH EACH WORD AND ADD TO THE HASHTABLE
+        for(int j = 0; j < numUniqueWords; j ++){
+                hashtable_add(hashtable, uniqueWords[j], absPath);
         }
-
-        // PRINT THE LIST
-        list_print(wordList);
 }
